2c5fbf3a021a2e5b03e729df85c23f573e0b02c1
Mark Boulton <mark@markboulton.co.uk>
2025-10-19 12:00:13 +0100

    Remove broken edge functions

--- netlify/edge-functions/test.ts
+++
@@ -1,7 +0,0 @@
-export default async () => {
-  return new Response('Edge Function is working!', {
-    headers: { 'content-type': 'text/plain' },
-  });
-};
-
-export const config = { path: "/test-edge" };
